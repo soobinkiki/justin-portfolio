2cc2a1b51bf335a91f5d0ef4cb0fea7ec8905690
soobinkiki <j.sb.parkk@gmail.com>
2021-04-27 20:00:28 -0700

    mobile responsive: checked

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Particles from 'react-particles-js'
 
 export default function App() {
     return (
         <>
+            <Particles 
+                className="particles"
+                params={{
+                    particles: {
+                        number: {
+                            value: 30,
+                            density: {
+                                enable: true,
+                                value_area: 1800
+                            }
+                        },
+                        shape: {
+                            type: "star",
+                            stroke: {
+                                width: 5,
+                                color: "#fff"
+                            }
+                        }
+                    }
+                }}/>
             <Navbar />
             <Header />
         </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-// import Typed from 'react-typed'
+import Typical from 'react-typical'
+import myphoto from '../../src/my_photo.jpeg'
 
 export default function Header () {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1>hello this is the header page</h1>
-                {/* <Typed /> */}
+                <h1>Hello, I'm Justin Park</h1>
+                <img id="my_photo" src={myphoto} alt="my_photo"/>
+                <h1>I'm a...{' '}</h1>
+                <h2>
+                    <Typical
+                        className="typical-text"
+                        loop={Infinity}
+                        wrapper="b"
+                        steps={[
+                            'Front End Developer 💻', 1500,
+                            'Software Engineer 💻', 1500,
+                            'Creative thinker 🤔', 1500
+                        ]}
+                    />
+                </h2>
             </div>
         </div>
     )
