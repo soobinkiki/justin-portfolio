974964cf09c172af281ee3aa8fa7d211dda93378
soobinkiki <j.sb.parkk@gmail.com>
2021-04-29 21:58:38 -0700

    project section done

--- src/components/Main/Project.jsx
+++ src/components/Main/Project.jsx
@@ -2,7 +2,43 @@ export default function Project () {
     return (
         <div className="project_container" id="project">
             <h1>Projects</h1>
+            <div className="gif_container">
+                <div className="gif">
+                    <img src="images/related.gif" alt="gif"/>
+                    <h2>RelateD</h2>
+                    <p>Developed the web app to connect the community of people who have disabilities and special needs with one another. 
+                        Served as a lead front-end developer in a team with 3 developers and created a full-stack web application using MERN stack 
+                        and wrote 80% codes on front-side functionalities.</p>
+                    <p>(HTML, CSS, MongoDB, Express.js, React.js, Node.js)
+                    </p>
+                    <div className="site_github_container">
+                        <a href="https://github.com/soobinkiki" target="_blank"><img id="githubIMG_2" src="/images/github.png" alt="GITHUB"></img></a>
+                        <a className="livesite" href="https://silly-swartz-2ee151.netlify.app" target="_blank">/  Live Site</a>
+                    </div>
+                </div>
+                <div className="gif">
+                    <img src="images/world_soccer_league.gif" alt="gif"/>
+                    <h2>World Soccer League</h2>
+                    <p>Created the web app to get the data of each of the countries' existing soccer leagues and participating clubs.</p>
+                    <p>(HTML, CSS, Node.js, Express.js, EJS, PostgreSQL) 
+                    </p>
+                    <div className="site_github_container">
+                        <a href="https://github.com/soobinkiki" target="_blank"><img id="githubIMG_2" src="/images/github.png" alt="GITHUB"></img></a>
+                        <a className="livesite" href="https://world-soccer-league.herokuapp.com/" target="_blank">/  Live Site</a>
+                    </div>
+                </div>
+                <div className="gif">
+                    <img src="images/numbers_memory_game.gif" alt="gif"/>
+                    <h2>Numbers Memory Game</h2>
+                    <p>Created the web app for users to test their memory. Two users take turns to type number(s) and challenge how many numbers they can memorize.</p>
+                    <p>(HTML, CSS, Javascript)
+                    </p>
+                    <div className="site_github_container">
+                        <a href="https://github.com/soobinkiki" target="_blank"><img id="githubIMG_2" src="/images/github.png" alt="GITHUB"></img></a>
+                        <a className="livesite" href="https://soobinkiki.github.io/" target="_blank">/  Live Site</a>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 }
