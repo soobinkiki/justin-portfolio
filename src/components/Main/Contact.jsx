e90f671c3464edac346ee63e8e06b31bec2ad17b
soobinkiki <j.sb.parkk@gmail.com>
2021-04-29 20:08:17 -0700

    contact styling done

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -1,15 +1,16 @@
+import emailLogo from '../../images/email.png'
 
 export default function Contact () {
 
     const sendMail = () => {
         const mailto = "mailto:j.sb.parkk@gmail.com?subject=Hi, Justin&body=How can I help you?";
-        // window.location.href = (mailto, "_blank");
         window.open(mailto, "_blank")
     }
 
     return (
-        <div className="contact_container" id="contact">
-            <a type="button" onClick={sendMail}><h1>Let's Talk</h1></a>
+        <div className="text-center d-flex align-items-center justify-content-center contact_main_container" id="contact" >
+            <button onClick={sendMail} id="contact_btn"><h1 className="lets_talk">Let's Talk</h1></button>
+            <a href="mailto:j.sb.parkk@gmail.com?subject=Hi, Justin&body=How can I help you?" target="_blank"><img id="email_logo" src={emailLogo} alt="email_logo"/></a>
         </div>
     )
 }
