c68b41163293dda1b9ccdec9f6b99cfa07c9f46f
soobinkiki <j.sb.parkk@gmail.com>
2021-05-03 19:13:26 -0700

    finished

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -25,7 +25,7 @@ export default function About () {
                         such as React, paired with CSS and bootstrap allow me to create well-designed and functioning solutions to problems.</p>
                     <div className="resume_linkedin_github">
                         <button id="resume" type="button"><a href="images/justin_resume.pdf" target="_blank">Resume</a></button>
-                        <a href="https://www.linkedin.com/in/justin-park/" target="_blank" rel="noreferrer"><img id="linkedInIMG" src="images/linkedin.png" alt="linkedIn"></img></a>
+                        <a href="https://www.linkedin.com/in/justin-sb-park/" target="_blank" rel="noreferrer"><img id="linkedInIMG" src="images/linkedin.png" alt="linkedIn"></img></a>
                         <a href="https://github.com/soobinkiki" target="_blank" rel="noreferrer"><img id="githubIMG" src="/images/github.png" alt="GITHUB"></img></a>
                     </div>
                 </div>
