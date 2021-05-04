eba7f5c0cbb5506177b55fedd750bfa797897e9f
soobinkiki <j.sb.parkk@gmail.com>
2021-05-03 17:20:53 -0700

    project: styling code edited

--- src/components/Main/Project.jsx
+++ src/components/Main/Project.jsx
@@ -2,16 +2,16 @@ export default function Project () {
     return (
         <div className="project_container" id="project">
             <h1>Projects</h1>
-            <div className="gif_container">
+            {/* <div className="gif_container"> */}
                 <div className="gif">
                     <div className="project_img">
                         <img src="images/related.gif" alt="gif"/>
                     </div>
                     <h2>RelateD</h2>
                     <div className="site_github_container">
-                        <a href="https://github.com/soobinkiki/relatedclient" target="_blank" rel="noreferrer"><span className="frontback" id="front-back">Frontend:</span><img id="githubIMG_3" src="/images/github.png" alt="GITHUB"></img></a>
-                        <a href="https://github.com/soobinkiki/relatedserver" target="_blank" rel="noreferrer"><span className="frontback">Backend:</span><img id="githubIMG_3" src="/images/github.png" alt="GITHUB"></img></a>
-                        <a className="livesite" href="https://silly-swartz-2ee151.netlify.app" target="_blank" rel="noreferrer">/  Live Site</a>
+                        <a href="https://github.com/soobinkiki/relatedclient" target="_blank" rel="noreferrer"><span className="frontback" id="front">Frontend:</span><img id="githubIMG_3" src="/images/github.png" alt="GITHUB"></img></a>
+                        <a href="https://github.com/soobinkiki/relatedserver" target="_blank" rel="noreferrer"><span className="frontback" id="back">Backend:</span><img id="githubIMG_3" src="/images/github.png" alt="GITHUB"></img></a>
+                        <span>/</span><a className="livesite" href="https://silly-swartz-2ee151.netlify.app" target="_blank" rel="noreferrer">Live Site</a>
                     </div>
                     <p>Developed the web app to connect the community of people who have disabilities and special needs with one another. 
                         Served as a lead front-end developer in a team with 3 developers and created a full-stack web application using MERN stack 
@@ -26,7 +26,7 @@ export default function Project () {
                     <h2>World Soccer League</h2>
                     <div className="site_github_container">
                         <a href="https://github.com/soobinkiki/World_Soccer_League" target="_blank" rel="noreferrer"><img id="githubIMG_2" src="/images/github.png" alt="GITHUB"></img></a>
-                        <a className="livesite" href="https://world-soccer-league.herokuapp.com/" target="_blank" rel="noreferrer">/  Live Site</a>
+                        <span>/</span><a className="livesite" href="https://world-soccer-league.herokuapp.com/" target="_blank" rel="noreferrer">Live Site</a>
                     </div>
                     <p>Created the web app to get the data of each of the countries' existing soccer leagues and participating clubs.</p>
                     <p>(HTML, CSS, Node.js, Express.js, EJS, PostgreSQL) 
@@ -39,12 +39,12 @@ export default function Project () {
                     <h2>Numbers Memory Game</h2>
                     <div className="site_github_container">
                         <a href="https://github.com/soobinkiki/Numbers-Memory-Game" target="_blank" rel="noreferrer"><img id="githubIMG_2" src="/images/github.png" alt="GITHUB"></img></a>
-                        <a className="livesite" href="https://soobinkiki.github.io/" target="_blank" rel="noreferrer">/  Live Site</a>
+                        <span>/</span><a className="livesite" href="https://soobinkiki.github.io/" target="_blank" rel="noreferrer">Live Site</a>
                     </div>
                     <p>Created the web app for users to test their memory. Two users take turns to type number(s) and challenge how many numbers they can memorize.</p>
                     <p>(HTML, CSS, Javascript)</p>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     )
 }
