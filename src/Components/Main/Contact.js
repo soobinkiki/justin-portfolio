28c80c7b69afd02cd9efd03e1fc26189958ce110
Justin Park <77993327+soobinkiki@users.noreply.github.com>
2021-09-15 00:24:11 -0700

    email text changed

--- src/Components/Main/Contact.js
+++ src/Components/Main/Contact.js
@@ -11,7 +11,7 @@ export default function Contact () {
     return (
         <div className="text-center d-flex align-items-center justify-content-center contact_main_container" id="contact" >
             <button onClick={sendMail} id="contact_btn"><h1 className="lets_talk">Let's Talk</h1></button>
-            <a href="mailto:j.sb.parkk@gmail.com?subject=Hello!&body=If you are reading this? You need to give me a job opportunity :)" 
+            <a href="mailto:j.sb.parkk@gmail.com?subject=Hello!&body=Hello recruiter, please give me an opportunity for the interview if I met your expectation!" 
                 target="_blank" rel="noreferrer"><img id="email_logo" src={Images.elogo} alt="email_logo"/></a>
         </div>
     )
